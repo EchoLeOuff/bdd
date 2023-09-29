2fd6d62cac3b9c15f52aca968d365f1e24776f45
EchoLeOuff <94800130+EchoLeOuff@users.noreply.github.com>
2023-09-29 09:43:33 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -9,19 +9,40 @@ def ouvrir_le_csv(nom_du_fichier, f):
         reader = csv.reader(f, dialect)
         donnees = list(reader)
         return donnees[1:]
+def reset():
+    conn = sqlite3.connect('bddclient.db')
+    cur = conn.cursor()
+    sql = "DELETE FROM commande"
+    sql2 = "DELETE FROM client"
+    cur.execute(sql)
+    cur.execute(sql2)
+    conn.commit()
+    print("Enregistrement supprimé avec succès")
+    cur.close()
+    conn.close()
 
 def commandes_bd(donnees_commandes):
-    con =
+    con = sqlite3.connect("bddclient.db")
+    cur = con.cursor()
     for L in donnees_commandes:
-       s=Template("""INSERT INTO commandes VALUES ($idclient, $idmeuble, $quantite, $date)""")
-       print(len(L))
+       s=Template("""INSERT INTO commande(idclient, idmeuble, quantite, date) VALUES ("$idclient", "$idmeuble", "$quantite", "$date")""")
        requete = s.substitute(idclient=L[0],idmeuble = L[1], quantite=L[2], date=L[3])
-       print(requete)
+       cur.execute(requete)
+       con.commit()
 
-
+def client_bd(donnees_client):
+    con = sqlite3.connect("bddclient.db")
+    cur = con.cursor()
+    for L in donnees_client:
+        s=Template("""INSERT INTO client(nom, prenom, adresse, ville, email, mdp) VALUES ("$nom","$prenom", "$adresse", "$ville", "$email", "$mdp")""")
+        requete = s.substitute(nom=L[0],prenom = L[1], adresse =L[2], ville=L[3], email = L[4], mdp = L[5])
+        cur.execute(requete)
+        con.commit()
 
 if __name__ == "__main__":
     donnees_commandes = ouvrir_le_csv("commandes.csv", "commandes")
-    donnees2 = ouvrir_le_csv("clients.csv", "client")
+    donnees_client = ouvrir_le_csv("clients.csv", "client")
     con = sqlite3.connect("bddclient.db")
+    reset()
     commandes_bd(donnees_commandes)
+    client_bd(donnees_client)
